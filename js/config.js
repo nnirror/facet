e614fb60160f3a3362809d4af1adb49d0e57d3a1
mjcella <michael.j.cella@gmail.com>
2023-11-12 11:27:41 -0500

    fix websocket port wonkiness, add css overflow hidden on editor body

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ let configSettings = {
   "OSC_OUTPORT": 5813,
   "SAMPLE_RATE": 44100,
   "EVENT_RESOLUTION_MS": 10,
-  "EDITOR_OSC_OUTPORT": 8081
+  "EDITOR_OSC_OUTPORT": 8080
 }
 
 if ( typeof module !== 'undefined' && typeof module.exports !== 'undefined' ) {
